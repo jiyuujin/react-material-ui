1bc83808cecc6af98310721b862a0f3a0ff9e2b1
jiyuujin <jiyuujinunite@gmail.com>
2021-05-28 10:40:38 +0900

    chore: ESLint, TypeScript-ESLint, and type definition

--- src/stories/button.stories.tsx
+++ src/stories/button.stories.tsx
@@ -4,7 +4,9 @@ import { action } from '@storybook/addon-actions'
 import { Button } from '../components/button'
 
 storiesOf('Element', module)
-    .add('Normal Button', () => <Button onClick={action('clicked')}>Button</Button>)
+    .add('Normal Button', () => (
+        <Button onClick={action('clicked')}>Button</Button>
+    ))
     .add('Primary Button', () => (
         <Button color={'primary'} onClick={action('clicked')}>
             Button
